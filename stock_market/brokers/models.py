a288ddc25e20c29e1a0f322a917424def33f630e
NistsiukAlexandr <nistsiuk.aliaksandr@pynest.io>
2023-12-13 10:53:45 +0300

    feat: create django apps models, add variables and path for media

--- stock_market/brokers/models.py
+++ stock_market/brokers/models.py
@@ -1,3 +1,138 @@
+from decimal import Decimal
+
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class InvestmentTypes(models.TextChoices):
+    STOCK = ("stock", "stock")
+    CRYPTOCURRENCY = ("cryptocurrency", "cryptocurrency")
+
+
+class OrderActivatedStatuses(models.TextChoices):
+    GTE = ("gte", "greater than or equal")
+    LTE = ("lte", "less than or equal")
+    GT = ("gt", "greater then")
+    LT = ("lt", "less than")
+    EQUAL = ("equal", "equal")
+
+
+class OrderStatuses(models.TextChoices):
+    ACTIVE = ("active", "active")
+    CANCELED = ("canceled", "canceled")
+    DELETED = ("deleted", "deleted")
+    COMPLETED = ("completed", "completed")
+
+
+class Investment(models.Model):
+    name = models.CharField(max_length=128, unique=True, db_index=True, null=False)
+    image = models.ImageField(upload_to="logos/", blank=True)
+    price = models.DecimalField(
+        null=False,
+        max_digits=settings.DECIMAL_MAX_DIGITS,
+        decimal_places=settings.DECIMAL_PLACES,
+    )
+    count = models.PositiveIntegerField(default=0)
+    type = models.CharField(choices=InvestmentTypes.choices, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "investment"
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(price__gte=Decimal("0")),
+                name="investment_price_non_negative",
+            ),
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    count = models.PositiveIntegerField(default=0)
+    status = models.CharField(
+        choices=OrderStatuses.choices, default=OrderStatuses.ACTIVE
+    )
+    is_sell = models.BooleanField(default=False)
+    owner = models.ForeignKey("InvestmentPortfolio", on_delete=models.CASCADE)
+    investment = models.ForeignKey("Investment", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class MarketOrder(Order):
+    class Meta:
+        db_table = "market_order"
+        ordering = ["created_at"]
+
+
+class LimitOrder(Order):
+    price = models.DecimalField(
+        null=False,
+        max_digits=settings.DECIMAL_MAX_DIGITS,
+        decimal_places=settings.DECIMAL_PLACES,
+    )
+    activated_status = models.CharField(
+        choices=OrderActivatedStatuses.choices, default=OrderActivatedStatuses.LTE
+    )
+
+    class Meta:
+        db_table = "limit_order"
+        ordering = ["created_at"]
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(price__gte=Decimal("0")),
+                name="limit_order_price_non_negative",
+            ),
+        ]
+
+
+class InvestmentPortfolio(models.Model):
+    count = models.PositiveIntegerField(default=0)
+    spend_amount = models.DecimalField(
+        null=False,
+        max_digits=settings.DECIMAL_MAX_DIGITS,
+        decimal_places=settings.DECIMAL_PLACES,
+    )
+    is_subscribed = models.BooleanField(default=True)
+    owner = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="portfolio"
+    )
+    investment = models.ForeignKey("Investment", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "investment_portfolio"
+        ordering = ["-count"]
+
+
+class Trade(models.Model):
+    count = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(
+        null=False,
+        max_digits=settings.DECIMAL_MAX_DIGITS,
+        decimal_places=settings.DECIMAL_PLACES,
+    )
+    seller = models.ForeignKey(
+        "InvestmentPortfolio", on_delete=models.DO_NOTHING, related_name="seller"
+    )
+    buyer = models.ForeignKey(
+        "InvestmentPortfolio", on_delete=models.DO_NOTHING, related_name="buyer"
+    )
+    investment = models.ForeignKey("Investment", on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "trade"
+        ordering = ["-created_at"]
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(price__gte=Decimal("0")), name="trade_price_non_negative"
+            ),
+        ]
--- stock_market/users/models.py
+++ stock_market/users/models.py
@@ -1,3 +1,101 @@
+from decimal import Decimal
+
+from django.conf import settings
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class Roles(models.TextChoices):
+    ADMIN = ("admin", "admin")
+    USER = ("user", "user")
+    ANALYST = ("analyst", "analyst")
+
+
+class UserManager(BaseUserManager):
+    def _create_user(self, email, username, password, **extra_fields):
+        if not all([email, username, password]):
+            raise ValueError(
+                "User must have an email address, an username and a password"
+            )
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+            **extra_fields,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, email, username, password):
+        return self._create_user(
+            email=email,
+            username=username,
+            password=password,
+            role=Roles.USER,
+        )
+
+    def create_superuser(self, email, password, username):
+        return self._create_user(
+            email=email,
+            username=username,
+            password=password,
+            role=Roles.ADMIN,
+        )
+
+    def create_analyst(self, email, password, username):
+        return self._create_user(
+            email=email,
+            username=username,
+            password=password,
+            role=Roles.ANALYST,
+        )
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True, db_index=True, null=False, max_length=255)
+    username = models.CharField(max_length=30, unique=True, db_index=True, null=False)
+    password = models.CharField(null=False, max_length=128)
+    role = models.CharField(choices=Roles.choices, default=Roles.USER, null=False)
+    image = models.ImageField(upload_to="avatars/", blank=True)
+    is_reset_password = models.BooleanField(default=False)
+    is_blocked = models.BooleanField(default=False)
+    balance = models.DecimalField(
+        max_digits=settings.DECIMAL_MAX_DIGITS,
+        decimal_places=settings.DECIMAL_PLACES,
+        default=Decimal("0"),
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    last_login = None
+
+    objects = UserManager()
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.role == Roles.ADMIN
+
+    class Meta:
+        db_table = "user"
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(balance__gte=Decimal("0")),
+                name="user_balance_non_negative",
+            ),
+        ]
+
+    def __str__(self):
+        return self.username
