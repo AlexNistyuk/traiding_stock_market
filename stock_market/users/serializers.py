a7664198ea7f88e3f3d1d8fff356e3a896e82e29
NistsiukAlexandr <nistsiuk.aliaksandr@pynest.io>
2023-12-14 16:23:43 +0300

    feat: add own creating method in UserCreateSerializer

--- stock_market/users/serializers.py
+++ stock_market/users/serializers.py
@@ -29,6 +29,15 @@ class UserCreateSerializer(serializers.ModelSerializer):
 
         write_only_fields = ("password",)
 
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
+
 
 class UserRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
