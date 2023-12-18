ffd34cd8041dcde5fb007b20cd7899027de068ba
NistsiukAlexandr <nistsiuk.aliaksandr@pynest.io>
2023-12-19 00:11:47 +0300

    fix: delete basename from user router

--- stock_market/api/v1/users.py
+++ stock_market/api/v1/users.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from users.views import UserChangePasswordViewSet, UserViewSet
 
 user_router = DefaultRouter()
-user_router.register(r"users", UserViewSet, basename="user")
+user_router.register(r"users", UserViewSet)
 
 password_router = DefaultRouter()
 password_router.register(r"change-password", UserChangePasswordViewSet)
